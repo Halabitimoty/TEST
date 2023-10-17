c13bec68b8d0ec7a3c256efe14fc31f7600b9499
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2023-10-17 16:39:57 +0100

    Task: Fix

--- Schema/shopItemSchema.js
+++ Schema/shopItemSchema.js
@@ -10,6 +10,10 @@ const shopItem = new mongoose.Schema(
       type: String,
       required: true,
     },
+    price: {
+      type: Number,
+      required: true,
+    },
     isInStock: {
       type: Boolean,
     },
